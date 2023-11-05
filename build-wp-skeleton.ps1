993a77045efdb301ed7493e22e396b86d45b6987
C-Elliott44 <C.Elliott.Dev@gmail.com>
2023-11-04 18:35:55 -0700

    reorganize or clean script

--- build-wp-skeleton.ps1
+++ build-wp-skeleton.ps1
@@ -1,6 +1,4 @@
-# Specify the base directory where you want to check for the directory
-$baseDirectory = "C:\xampp\htdocs"
-
+# ------------------------------ USER INPUT ------------------------------
 # Get the client's domain and password from the user
 $clientDomain = Read-Host "Enter the client's domain (e.g., example.com)"
 $clientPassword = Read-Host "Enter the client's password (e.g., wxa.kgj4mgw2MYB4zhg)"
@@ -24,32 +22,111 @@ if ([string]::IsNullOrEmpty($clientPrefix)) {
     exit
 }
 
-# Define the full path to the directory
-$directoryPath = Join-Path -Path $baseDirectory -ChildPath $clientDomain
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ VARIABLES ------------------------------
+$dhaliWebsitesDir = "C:\xampp\htdocs"                                                   # Directory where Cameron Codes
+$wpCoreURL = "http://wordpress.org/latest.zip"                                          # URL for WordPress Download
+$themeURL = "https://github.com/Automattic/_s.git"                                      # URL for Theme To Download for Site
+$finalDirLocation = Join-Path -Path $dhaliWebsitesDir -ChildPath $clientDomain          # Final Location of PowerShell Output
+$clientDomainNoExtention = $clientDomain -replace '\..*$', ''                           # Client's Domain without Extention
+
+$allThemesDir = Join-Path $PSScriptRoot "$clientDomain\wp-content\themes"               # themes dir Folder Location
+$themeDir = "$allThemesDir\$clientDomainNoExtention"                                    # Downloaded Theme Folder Location
+
+$wpSampleConfigFile = "$clientDomain/wp-config-sample.php"                              # wp-config-sample.php File Location
+$wpConfigFile = "$PSScriptRoot\$clientDomain\wp-config.php"                             # wp-config.php File Location
+$wpConfigDatabase = $clientDomainNoExtention + "_main"                                  # wp-config.php Database Name
+$wpConfigUsername = $clientDomainNoExtention + "_admin"                                 # wp-config.php Username Name
+$oldSalts = @"
+define( 'AUTH_KEY',         'put your unique phrase here' );
+define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
+define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
+define( 'NONCE_KEY',        'put your unique phrase here' );
+define( 'AUTH_SALT',        'put your unique phrase here' );
+define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
+define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
+define( 'NONCE_SALT',       'put your unique phrase here' );
+"@                                                                                      # Salts in wp-config.php to REPLACE
+$saltUrl = "https://api.wordpress.org/secret-key/1.1/salt/"                             # URL for WP Salts
+
+$themeTextDomain = "'$clientDomainNoExtention'"                                         # Underscore "Find & Replace" variables
+$themeFunctions = $clientDomainNoExtention + "_"                                        # Underscore "Find & Replace" variables
+$themeStyleDomain = "Text Domain: " + $clientDomainNoExtention                          # Underscore "Find & Replace" variables
+$themeDocBlocks = " $clientDomainNoExtention"                                           # Underscore "Find & Replace" variables
+$themePrefixHandlers = "$clientDomainNoExtention-"                                      # Underscore "Find & Replace" variables
+$themeConstants = $clientDomainNoExtention.ToUpper() + "_"                              # Underscore "Find & Replace" variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ Check htdocs (FOLDER SHOULD NOT EXIST - IF IT DOES... RENAME IT!) ------------------------------
 # Check if the directory exists
-if (Test-Path -Path $directoryPath -PathType Container) {
+if (Test-Path -Path $finalDirLocation -PathType Container) {
     # The directory exists, so prompt the user
     $choice = Read-Host "Directory '$clientDomain' already exists. Do you want to rename it to '$clientDomain-old'? (Y/N)"
 
     if ($choice -eq "Y" -or $choice -eq "y") {
         # Rename the existing directory to add "-old"
-        $newDirectoryPath = Join-Path -Path $baseDirectory -ChildPath "$clientDomain-old"
-        Rename-Item -Path $directoryPath -NewName $newDirectoryPath
-        Write-Host "Directory in $baseDirectory renamed to '$clientDomain-old'."
+        $newDirectoryPath = Join-Path -Path $dhaliWebsitesDir -ChildPath "$clientDomain-old"
+        Rename-Item -Path $finalDirLocation -NewName $newDirectoryPath
+        Write-Host "Directory in $dhaliWebsitesDir renamed to '$clientDomain-old'."
     } else {
         Write-Host "No changes were made. Exiting..."
         exit
     }
 } else {
-    Write-Host "Directory '$clientDomain' is avalible in '$baseDirectory'. script continuing..."
+    Write-Host "Directory '$clientDomain' is avalible in '$dhaliWebsitesDir'. script continuing..."
 }
 
-# Define the download URL for the latest WordPress core
-$wordpressDownloadUrl = "http://wordpress.org/latest.zip"
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ WP CORE .ZIP - DOWNLOAD, EXPAND, RENAME, & DELETE OLD ZIP ------------------------------
 # Download the latest WordPress core zip file
-Invoke-WebRequest -Uri $wordpressDownloadUrl -OutFile "$PSScriptRoot\latest.zip"
+Invoke-WebRequest -Uri $wpCoreURL -OutFile "$PSScriptRoot\latest.zip"
 
 # Extract the zip file to the destination folder
 Expand-Archive -Path "$PSScriptRoot\latest.zip" -DestinationPath $PSScriptRoot
@@ -62,59 +139,61 @@ Remove-Item -Path "$PSScriptRoot\latest.zip"
 
 Write-Host "WordPress has been downloaded and extracted to the folder: $clientDomain"
 
-$sourceFile = "$clientDomain/wp-config-sample.php"  # Replace with the path to your source file
-$destinationFolder = "$clientDomain"    # Replace with the path to your "site-folder"
 
-# The new name for the copied file
-$newFileName = "wp-config.php"
 
-# Combine the destination folder path with the new filename
-$destinationPath = Join-Path -Path $destinationFolder -ChildPath $newFileName
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ CREATE WP-CONFIG.PHP FILE ------------------------------
 try {
     # Copy the source file to the destination folder with the new name
-    Copy-Item -Path $sourceFile -Destination $destinationPath
-    Write-Host "File copied to $destinationPath"
+    Copy-Item -Path $wpSampleConfigFile -Destination $wpConfigFile
+    Write-Host "File copied to $wpConfigFile"
 } catch {
     Write-Host "An error occurred: $_"
 }
 
-# Remove everything from the period to the end
-$clientDomainNoExtention = $clientDomain -replace '\..*$', ''
 
-# Set concatinate config.php values
-$database = $clientDomainNoExtention + "_main"
-$username = $clientDomainNoExtention + "_admin"
 
-# Define the file path
-$filePath = "$PSScriptRoot\$clientDomain\wp-config.php"
 
-# Read the content of the file into a variable
-# Use {-Raw} at the end of a {Get-Content} in order to do multiline find and replace
-$content = Get-Content -Path $filePath -Raw
 
-# Define the URL
-$saltUrl = "https://api.wordpress.org/secret-key/1.1/salt/"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ EDIT WP-CONFIG.PHP FILE ------------------------------
+
+# RAW content output of wp-config.php File
+$rawWpConfigContent = Get-Content -Path $wpConfigFile -Raw
 
 # Use Invoke-RestMethod to retrieve the content from the saltURL
 $newSalts = Invoke-RestMethod -Uri $saltUrl
 
-# Set the old salts to be found and replaced
-$oldSalts = @"
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-"@
-
 # Define an array of replacement patterns and corresponding new text
 $replacements = @(
-    @("database_name_here", $database),
-    @("username_here", $username),
+    @("database_name_here", $wpConfigDatabase),
+    @("username_here", $wpConfigUsername),
     @("password_here", $clientPassword),
     @("'wp_'", "'$clientPrefix'"),
     @($oldSalts, $newSalts)
@@ -123,37 +202,37 @@ $replacements = @(
 # Loop through each replacement pattern and update the content
 foreach ($replacement in $replacements) {
     $oldText, $newText = $replacement
-    $content = $content -replace [regex]::Escape($oldText), $newText
+    $rawWpConfigContent = $rawWpConfigContent -replace [regex]::Escape($oldText), $newText
 }
 
 # Write the modified content back to the file
-$content | Set-Content -Path $filePath
+$rawWpConfigContent | Set-Content -Path $wpConfigFile
 
-# Define the GitHub repository URL and the target directory
-$githubRepoUrl = "https://github.com/Automattic/_s.git"
-$targetDirectory = Join-Path $PSScriptRoot "$clientDomain\wp-content\themes"
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------ DOWNLOAD UNDERSCORES THEME ------------------------------
 
 # Create the target directory if it doesn't exist
-if (-not (Test-Path -Path $targetDirectory -PathType Container)) {
-    New-Item -Path $targetDirectory -ItemType Directory -Force
+if (-not (Test-Path -Path $allThemesDir -PathType Container)) {
+    New-Item -Path $allThemesDir -ItemType Directory -Force
 }
 
 # Clone the GitHub repository into the target directory
-git clone $githubRepoUrl $targetDirectory\_s
+git clone $themeURL $allThemesDir\_s
 
 # Rename the folder to $ClientsDomain
-Rename-Item -Path (Join-Path $targetDirectory "_s") -NewName (Join-Path $targetDirectory $clientDomainNoExtention) -Force
-
-# Define the directory where your files are located
-$underScoresDirectory = "$targetDirectory\$clientDomainNoExtention"
-
-# Define an array of find and replace pairs
-$themeTextDomain = "'$clientDomainNoExtention'"
-$themeFunctions = $clientDomainNoExtention + "_"
-$themeStyleDomain = "Text Domain: " + $clientDomainNoExtention
-$themeDocBlocks = " $clientDomainNoExtention"
-$themePrefixHandlers = "$clientDomainNoExtention-"
-$themeConstants = $clientDomainNoExtention.ToUpper() + "_"
+Rename-Item -Path (Join-Path $allThemesDir "_s") -NewName (Join-Path $allThemesDir $clientDomainNoExtention) -Force
 
 $findReplacePairs = @(
     @{"Find" = "'_s'"; "Replace" = $themeTextDomain},
@@ -165,7 +244,7 @@ $findReplacePairs = @(
 )
 
 # Get a list of files in the specified directory and its subdirectories
-$files = Get-ChildItem -Path $underScoresDirectory -File -Recurse
+$files = Get-ChildItem -Path $themeDir -File -Recurse
 
 # Iterate through each file
 foreach ($file in $files) {
@@ -212,7 +291,7 @@ Write-Host "Text replacements complete."
 # # Check if the directory exists in the current root directory
 # if (Test-Path -Path $directoryToMove -PathType Container) {
 #     # Specify the target directory where you want to move the folder
-#     $targetDirectory = Join-Path -Path $baseDirectory -ChildPath $clientDomain
+#     $targetDirectory = Join-Path -Path $dhaliWebsitesDir -ChildPath $clientDomain
 
 #     # Move the folder to the target directory
 #     Move-Item -Path $directoryToMove -Destination $targetDirectory -Force
